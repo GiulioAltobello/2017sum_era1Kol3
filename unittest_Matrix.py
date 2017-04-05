07d69a356d7c61c79ce8f116c435b3af502c7425
GiulioAltobello <g89.10@hotmail.it>
2017-04-05 14:50:06 +0200

    Add files via upload

--- unittest_Matrix.py
+++ unittest_Matrix.py
@@ -8,18 +8,18 @@ class test_matrix(unittest.TestCase):
     def test_on_int_prod_operation_method_correct_result(self):
         m2=matrix([[1,2,3],[1,2,3],[1,2,3]])
         result=matrix([[3,6,9],[3,6,9],[3,6,9]])
-        self.assertEqual(result, self.mat.prod(m2))
+        self.assertEqual(result.mat, (self.mat.prod(m2)).mat)
     
     def test_on_float_prod_operation_method_correct_result(self):
         m2=matrix([[1.5,2.5,3.5],[1.5,2.5,3.5],[1.5,2.5,3.5]])
         result=matrix([[4.5,7.5,10.5 ],[4.5,7.5,10.5],[4.5,7.5,10.5]])
-        self.assertEqual(result, self.mat.prod(m2))
+        self.assertEqual(result.mat, (self.mat.prod(m2)).mat)
     
     
     def test_on_int_add_operation_method_correct_result(self):
         m2=matrix([[1,2,3],[1,2,3],[1,2,3]])
         result=matrix([[3,6,9],[3,6,9],[3,6,9]])
-        self.assertEqual(result, self.mat+m2)
+        self.assertEqual(result.mat, (self.mat+m2).mat)
         
 if __name__=='__main__':
     unittest.main()
